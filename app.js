530cd50461178c458366af034baa40e7403d5da7
Pierre-Alexandre Monneau <pierre-alexandre.monneau@epitech.eu>
2017-12-19 17:55:55 +0100

    init repo

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var path = require('path');
-
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://yeah-boy.com:30000/test');
 app.set('view engine', 'pug');
 
 http.createServer(app).listen(app.get(8080), function(){
