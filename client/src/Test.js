d871bd8311e0175bd8ab8526e59c9848d3b98dea
Pierre-Alexandre Monneau <pierre-alexandre.monneau@epitech.eu>
2018-01-02 07:46:32 +0100

    api/client

---
+++ client/src/Test.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="Test">
+        <header className="Test-header">
+          <h1 className="Test-title">Je suis god like</h1>
+        </header>
+        <p className="Test-intro">
+           bonjour
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Test;
